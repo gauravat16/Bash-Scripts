3d48174f1c362aa94bc8af0b6091e2087d4101c4
Gaurav Sharma <dev.gaurav94@gmail.com>
2018-11-30 00:49:30 +0530

    cleanup

--- ScriptOptions.sh
+++ ScriptOptions.sh
@@ -1,53 +1,45 @@
+#Example
+OPTION_1=""
+option_array[0]="1"
+option_array[1]="2"
 
-	#To add a new option add entry in both case statements
-	#We look through the options as we can have multiple options
-
-  #Example
-  OPTION_1=""
-  option_array[0]="1"
-	option_array[1]="2"
-  
-	options="$@"
- 
-  
-  set_options(){
-	prev_option=""
-
-	for option in $options;
-	do
-		case $prev_option in
-			"-t" )
-			OPTION_1=$option	
-			prev_option=""	
-				;;
-			"-d" )
-				option_array+=("$option")
-				prev_option=""	
-				;;
-			"-g" )
-
-				option_array_2+=("$option")			
-				prev_option=""	
-				;;
-
-		esac
-
-		case $option in
-			"-t" )
-				prev_option="-t"
-
-				;;
+options="$@"
 
 
-			"-d" )
-				prev_option="-d"
-				;;
+set_options(){
+    prev_option=""
+    
+    for option in $options;
+    do
+        case $prev_option in
+            "-t" )
+                OPTION_1=$option
+            ;;
+            "-d" )
+                option_array+=("$option")
+            ;;
+            "-g" )
+                
+                option_array_2+=("$option")
+            ;;
+            
+        esac
+        
+        prev_option="$option"
+        
+    done
+    
+}
 
-			"-g" )
-				prev_option="-g"
-				;;
+function test()
+{
+	set_options
 
-		esac
-	done
+	echo $OPTION_1
+	echo ${option_array[*]}
+	echo ${option_array_2[*]}
+	
+}
 
-	}
+
+test
