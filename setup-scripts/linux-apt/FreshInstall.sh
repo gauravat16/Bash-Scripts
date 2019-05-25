7c71d8af961858895180d6943ad5bbf969ee9c0d
gauravat16 <dev.gaurav94@gmail.com>
2019-05-26 00:46:42 +0530

    Added docker and made it automatic

--- setup-scripts/linux-apt/FreshInstall.sh
+++ setup-scripts/linux-apt/FreshInstall.sh
@@ -1,46 +1,120 @@
-#Vars
-SOFTWARES="$HOME/softwares"
-INSTALLATIONS="$HOME/installs"
+#Colors
+	RED='\033[0;31m'
+	WHITE='\033[97m'
+	NC='\033[0m'
+	BOLD='\033[1m'
+	GREEN='\033[92m'
+	DIM='\033[2m'
+	STOP='\033[0m'
 
-#Set up Folder
-mkdir -p $SOFTWARES
-mkdir -p $INSTALLATIONS
 
-#Get Updated
-sudo apt-get update && sudo apt-get upgrade
 
 #install chrome
-wget -v https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P $SOFTWARES
-sudo dpkg -i  $SOFTWARES/google-chrome-stable_current_amd64.deb
+function installChrome(){
+    wget -v https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P $SOFTWARES
+    sudo dpkg -i  $SOFTWARES/google-chrome-stable_current_amd64.deb
+}
+
 
 #install MySQL 5.7
-sudo apt-get install mysql-server-5.7
+function installMySql(){
+    sudo apt-get install mysql-server-5.7
+}
 
 #install TLP
-sudo add-apt-repository ppa:linrunner/tlp
-sudo apt-get update
-sudo apt-get install tlp tlp-rdw
-sudo tlp start
+function installTLP(){
+    sudo add-apt-repository ppa:linrunner/tlp
+    sudo apt-get update
+    sudo apt-get install tlp tlp-rdw
+    sudo tlp start
+}
 
 #Download Eclipse and STS
-sudo snap install --classic eclipse
+function installEclipseBasedIde(){
+    sudo snap install --classic eclipse
+    wget -v https://download.springsource.com/release/STS/3.9.8.RELEASE/dist/e4.11/spring-tool-suite-3.9.8.RELEASE-e4.11.0-linux-gtk-x86_64.tar.gz -P $SOFTWARES
+    tar -xzf ~/Dev/spring-tool-suite-3.9.8.RELEASE-e4.11.0-linux-gtk-x86_64.tar.gz  -C $INSTALLATIONS
+}
 
-wget -v https://download.springsource.com/release/STS/3.9.8.RELEASE/dist/e4.11/spring-tool-suite-3.9.8.RELEASE-e4.11.0-linux-gtk-x86_64.tar.gz -P $SOFTWARES
-
-tar -xzf ~/Dev/spring-tool-suite-3.9.8.RELEASE-e4.11.0-linux-gtk-x86_64.tar.gz  -C $INSTALLATIONS
 
 #install java8
-sudo apt update
-sudo apt install openjdk-8-jdk
+function installJDK(){
+    sudo apt update
+    sudo apt install openjdk-8-jdk
+}
+
 
 #MySQL Workbench
-sudo apt-get install mysql-workbench
+function installWorkbench(){
+    sudo apt-get install mysql-workbench
+}
 
 #Android Studio
-sudo snap install android-studio --classic
+function installAndroidStudio(){
+    sudo snap install android-studio --classic
+}
 
 #VSCode
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+function installVSCode(){
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt install code
+}
 
+#Docker
+function installDocker(){
+installDockerCE
+installDockerCompose
+}
+
+function installDockerCE(){
+sudo apt-get update
+ sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+       sudo apt update
+   sudo apt-get install docker-ce docker-ce-cli containerd.io
+}
+
+function installDockerCompose(){
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+}
+
+function init(){
+    #Vars
+    SOFTWARES="$HOME/softwares"
+    INSTALLATIONS="$HOME/installs"
+    
+    
+    #Get Updated
+    #sudo apt-get update && sudo apt-get upgrade
+
+    #Set up Folder
+    mkdir -p $SOFTWARES
+    mkdir -p $INSTALLATIONS
+    
+    for fn in $(declare -F)
+    do
+      if [[ $fn == install* ]]
+      then
+        echo -e "$GREEN""\nRunning installation subroutine - [$fn]........ $STOP\n"
+        time $fn
+        echo -e "$GREEN""\n[$fn] - Done!$STOP\n"
+
+      fi
+    done
+
+}
+
+init
