e2a49c1402c461fc54aa4590e59c58bec88a7133
Gaurav <gaurav@FH-MAC-19-57.local>
2019-06-07 15:01:38 +0530

    macos web setup added

---
+++ setup-scripts/macos-brew/setup_web_env.sh
@@ -0,0 +1,80 @@
+#Colors
+	RED='\033[0;31m'
+	WHITE='\033[97m'
+	NC='\033[0m'
+	BOLD='\033[1m'
+	GREEN='\033[92m'
+	DIM='\033[2m'
+	STOP='\033[0m'
+
+#install command line tools
+function setupCommandLineTools(){
+    xcode-select --install
+}
+
+#install brew
+function setupHomeBrew(){
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install wget
+}
+
+#install jdk8
+function installJDK8(){
+    brew tap AdoptOpenJDK/openjdk
+    brew cask install adoptopenjdk8
+}
+
+#install chrome
+function installChrome(){
+    brew cask install google-chrome
+}
+
+#install vscode
+function installChrome(){
+    brew cask install visual-studio-code
+}
+
+#install sts
+function installSTS(){
+    wget -v https://download.springsource.com/release/STS4/4.2.2.RELEASE/dist/e4.11/spring-tool-suite-4-4.2.2.RELEASE-e4.11.0-macosx.cocoa.x86_64.dmg -P $SOFTWARES
+    sudo hdiutil attach ~/Dev/spring-tool-suite-4-4.2.2.RELEASE-e4.11.0-macosx.cocoa.x86_64.dmg
+}
+
+#install docker
+function installDocker(){
+    wget -v https://download.docker.com/mac/stable/Docker.dmg -P $SOFTWARES
+    sudo hdiutil attach ~/Dev/Docker.dmg
+}
+
+#install postman
+function installPostman(){
+    brew cask install postman
+}
+
+function init(){
+    
+    setupCommandLineTools
+    setupHomeBrew
+
+    #Vars
+    SOFTWARES="$HOME/softwares"
+    INSTALLATIONS="$HOME/installs"
+
+    #Set up Folder
+    mkdir -p $SOFTWARES
+    mkdir -p $INSTALLATIONS
+    
+    for fn in $(declare -F)
+    do
+      if [[ $fn == install* ]]
+      then
+        echo -e "$GREEN""\nRunning installation subroutine - [$fn]........ $STOP\n"
+        time $fn
+        echo -e "$GREEN""\n[$fn] - Done!$STOP\n"
+
+      fi
+    done
+
+}
+
+init
